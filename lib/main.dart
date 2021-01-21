475a4035c4bb337355c250dc6c4f4824a83f4639
Mike Zamayias <mzamagias@gmail.com>
2021-01-22 00:22:05 +0200

    updated main.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-import 'views/current.dart';
+import 'views/game.dart';
+import 'views/options.dart';
+import 'views/help.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,6 +15,38 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+  static const TextStyle _optionStyle = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.bold,
+  );
+  static const List<Widget> _widgets = <Widget>[
+    GameView(
+      widgetText: Text(
+        'Game View',
+        style: _optionStyle,
+      ),
+    ),
+    OptionsView(
+      widgetText: Text(
+        'Options View',
+        style: _optionStyle,
+      ),
+    ),
+    HelpView(
+      widgetText: Text(
+        'Help View',
+        style: _optionStyle,
+      ),
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     double padding = 20.0;
@@ -29,10 +63,31 @@ class _MyAppState extends State<MyApp> {
         ),
         child: Scaffold(
           backgroundColor: Colors.grey[200],
+          bottomNavigationBar: BottomNavigationBar(
+            items: <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.games_rounded),
+                label: 'Game',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.settings_rounded),
+                label: 'Options',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.help_rounded),
+                label: 'Help',
+              )
+            ],
+            currentIndex: _selectedIndex,
+            selectedItemColor: Colors.blueAccent,
+            unselectedItemColor: Colors.grey[500],
+            onTap: _onItemTapped,
+          ),
           body: SafeArea(
-            child: Padding(
-              padding: EdgeInsets.fromLTRB(0, padding, 0.0, padding),
-              child: CurrentView(),
+            child: Center(
+              child: _widgets.elementAt(
+                _selectedIndex,
+              ),
             ),
           ),
         ),
