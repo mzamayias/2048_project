41d6982757be05105df76767056f10b7a0d1cbb9
Mike Zamayias <mzamagias@gmail.com>
2021-01-22 18:18:37 +0200

    refactored bottom navigation bar code

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-import 'views/game.dart';
-import 'views/options.dart';
-import 'views/help.dart';
+import 'views/current.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
+  const MyApp({Key key}) : super(key: key);
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  int _selectedIndex = 0;
-  static const TextStyle _optionStyle = TextStyle(
-    fontSize: 30,
-    fontWeight: FontWeight.bold,
-  );
-  static const List<Widget> _widgets = <Widget>[
-    GameView(
-      widgetText: Text(
-        'Game View',
-        style: _optionStyle,
-      ),
-    ),
-    OptionsView(
-      widgetText: Text(
-        'Options View',
-        style: _optionStyle,
-      ),
-    ),
-    HelpView(
-      widgetText: Text(
-        'Help View',
-        style: _optionStyle,
-      ),
-    ),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    double padding = 20.0;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: AnnotatedRegion<SystemUiOverlayStyle>(
@@ -61,36 +27,7 @@ class _MyAppState extends State<MyApp> {
           systemNavigationBarIconBrightness: Brightness.dark,
           systemNavigationBarDividerColor: Colors.transparent,
         ),
-        child: Scaffold(
-          backgroundColor: Colors.grey[200],
-          bottomNavigationBar: BottomNavigationBar(
-            items: <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.games_rounded),
-                label: 'Game',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.settings_rounded),
-                label: 'Options',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.help_rounded),
-                label: 'Help',
-              )
-            ],
-            currentIndex: _selectedIndex,
-            selectedItemColor: Colors.blueAccent,
-            unselectedItemColor: Colors.grey[500],
-            onTap: _onItemTapped,
-          ),
-          body: SafeArea(
-            child: Center(
-              child: _widgets.elementAt(
-                _selectedIndex,
-              ),
-            ),
-          ),
-        ),
+        child: CurrentView(),
       ),
     );
   }
--- lib/views/current.dart
+++ lib/views/current.dart
@@ -2,12 +2,84 @@ import 'package:flutter/material.dart';
 
 import 'game.dart';
 import 'options.dart';
+import 'help.dart';
 
-class CurrentView extends StatelessWidget {
+class CurrentView extends StatefulWidget {
   const CurrentView({Key key}) : super(key: key);
+  @override
+  _CurrentViewState createState() => _CurrentViewState();
+}
+
+class _CurrentViewState extends State<CurrentView> {
+  int _selectedIndex = 0;
+  static const TextStyle _optionStyle = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.bold,
+  );
+  static const List<Widget> _widgets = <Widget>[
+    GameView(
+      widgetText: Text(
+        'Game View',
+        style: _optionStyle,
+      ),
+    ),
+    OptionsView(
+      widgetText: Text(
+        'Options View',
+        style: _optionStyle,
+      ),
+    ),
+    HelpView(
+      widgetText: Text(
+        'Help View',
+        style: _optionStyle,
+      ),
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.gamepad_outlined),
+            activeIcon: Icon(Icons.gamepad_rounded),
+            label: 'Game',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings_outlined),
+            activeIcon: Icon(Icons.settings_rounded),
+            label: 'Options',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.help_outline_rounded),
+            activeIcon: Icon(Icons.help_rounded),
+            label: 'Help',
+          )
+        ],
+        onTap: _onItemTapped,
+        currentIndex: _selectedIndex,
+        iconSize: 32,
+        selectedItemColor: Colors.blueAccent[400],
+        unselectedItemColor: Colors.grey[500],
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+      ),
+      body: SafeArea(
+        child: Center(
+          child: _widgets.elementAt(
+            _selectedIndex,
+          ),
+        ),
+      ),
+    );
   }
 }
