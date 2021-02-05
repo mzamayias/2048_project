742d596e00632048b9bc12ba2e978604e2e7d74b
Mike Zamayias <mzamagias@gmail.com>
2021-02-05 03:20:56 +0200

    some refactoring

---
+++ lib/views/game/exit/components/layouts/exit_layout.dart
@@ -0,0 +1,87 @@
+// import packages
+import 'package:flutter/material.dart';
+
+// import models
+import 'package:project_2048/models/help/about/app.dart';
+import 'package:project_2048/models/help/about/developer.dart';
+
+// import view components
+import 'package:project_2048/views/components/base_view.dart';
+import 'package:project_2048/views/components/base_card.dart';
+import 'package:project_2048/views/components/base_button.dart';
+import 'package:project_2048/views/help/about/components/about_data_card.dart';
+
+class AboutViewLayout extends StatelessWidget {
+  const AboutViewLayout({
+    Key key,
+    @required this.developer,
+    @required this.app,
+  }) : super(key: key);
+
+  final Developer developer;
+  final App app;
+
+  @override
+  Widget build(BuildContext context) {
+    return BaseView(
+      viewTitle: 'About',
+      children: <Widget>[
+        BaseCard(
+          cardText: 'Developer details',
+        ),
+        Expanded(
+          flex: 1,
+          child: SizedBox(
+            height: 1,
+          ),
+        ),
+        AboutDataCard(
+          flex: 8,
+          dataType: 'Name',
+          dataValue: '${developer.firstName} ${developer.lastName}',
+        ),
+        Expanded(
+          flex: 1,
+          child: SizedBox(
+            height: 1,
+          ),
+        ),
+        BaseCard(
+          cardText: 'App details',
+        ),
+        Expanded(
+          flex: 1,
+          child: SizedBox(
+            height: 1,
+          ),
+        ),
+        AboutDataCard(
+          flex: 8,
+          dataType: 'Name',
+          dataValue: '${app.name}',
+        ),
+        Expanded(
+          flex: 1,
+          child: SizedBox(
+            height: 1,
+          ),
+        ),
+        AboutDataCard(
+          flex: 8,
+          dataType: 'Version',
+          dataValue: '${app.version}',
+        ),
+        Expanded(
+          flex: 1,
+          child: SizedBox(
+            height: 1,
+          ),
+        ),
+        BaseButton(
+          buttonText: 'Go back',
+          onPressed: () => Navigator.pop(context),
+        ),
+      ],
+    );
+  }
+}
--- lib/views/game/game_view.dart
+++ lib/views/game/game_view.dart
@@ -3,6 +3,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:project_2048/controllers/game/exit/exit_app.dart';
 
 // import view components
 import 'package:project_2048/views/components/base_button.dart'; // blue buttons
@@ -19,9 +20,6 @@ class GameView extends StatelessWidget {
     return BaseView(
       viewTitle: 'Game',
       children: <Widget>[
-        SizedBox(
-          height: 18,
-        ),
         BaseButton(
           buttonText: 'New game',
         ),
@@ -36,41 +34,7 @@ class GameView extends StatelessWidget {
         ),
         BaseButton(
           buttonText: 'Exit',
-          onPressed: () =>
-              showDialog(
-                barrierDismissible: true,
-                barrierColor: Colors.grey[50],
-                context: context,
-                builder: (context) => AlertDialog(
-                  title: Text(
-                    'Are you sure you want to exit the app?',
-                    style: TextStyle(
-                      color: Colors.grey[850],
-                      fontWeight: FontWeight.bold,
-                      fontSize: 21,
-                    ),
-                  ),
-                  elevation: 0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                  ),
-                  backgroundColor: Colors.grey[50],
-                  titlePadding: EdgeInsets.all(0.0),
-                  buttonPadding: EdgeInsets.all(21.0),
-                  actions: <Widget>[
-                    ExitButton(
-                      onPressed: () => Navigator.of(context).pop(false),
-                      buttonText: 'No',
-                    ),
-                    ExitButton(
-                      onPressed: () => exit(0),
-                      // onPressed: () => Navigator.of(context).pop(false),
-                      buttonText: 'Yes',
-                    ),
-                  ],
-                ),
-              ) ??
-              false,
+          onPressed: () => exitApp(context),
         ),
       ],
     );
