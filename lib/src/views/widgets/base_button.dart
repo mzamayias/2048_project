7053afe9e5ecf63a28cafa88ac07b3bb67045e42
Mike Zamayias <mzamagias@gmail.com>
2021-02-09 07:42:29 +0200

    rewritten

--- lib/src/views/widgets/base_button.dart
+++ lib/src/views/widgets/base_button.dart
@@ -3,31 +3,41 @@ import 'package:flutter/material.dart';
 class BaseButton extends StatelessWidget {
   const BaseButton({
     Key key,
-    this.buttonText,
+    this.flex,
+    @required this.buttonText,
     this.onPressed,
   }) : super(key: key);
 
+  final int flex;
   final String buttonText;
   final Function onPressed;
 
   @override
   Widget build(BuildContext context) {
-    return FlatButton(
-      onPressed: onPressed ?? () => print('"$buttonText" pressed!'),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(21)),
-      ),
-      color: Colors.blueAccent[700],
-      splashColor: Colors.grey[850].withOpacity(0.3),
-      child: Padding(
-        padding: EdgeInsets.all(21),
-        child: Text(
-          buttonText,
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Colors.grey[50],
-            fontSize: 21,
-            fontWeight: FontWeight.bold,
+    return GestureDetector(
+      onTap: onPressed ?? () => print('"$buttonText" pressed!'),
+      child: Expanded(
+        flex: flex ?? 1,
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(21)),
+          ),
+          color: Colors.blueAccent[700],
+          elevation: 0,
+          shadowColor: Colors.transparent,
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.all(21),
+              child: Text(
+                buttonText,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.grey[50],
+                  fontSize: 21,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
           ),
         ),
       ),
