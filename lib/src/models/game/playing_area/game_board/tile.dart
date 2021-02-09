136fadaa756144dbb7ba79509815f686653d42c5
Mike Zamayias <mzamagias@gmail.com>
2021-02-09 20:11:53 +0200

    started implementing gameboard and tiles

---
+++ lib/src/models/game/playing_area/game_board/tile.dart
@@ -0,0 +1,13 @@
+class Tile {
+  int row;
+  int column;
+  int value;
+  bool isMerged;
+
+  Tile({
+    this.row,
+    this.column,
+    this.value,
+    this.isMerged,
+  });
+}
--- lib/src/views/game/playing_area/game_board/game_board_view.dart
+++ lib/src/views/game/playing_area/game_board/game_board_view.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:project_2048/src/views/widgets/one_string_card.dart';
 
 class GameBoardView extends StatelessWidget {
   const GameBoardView({Key key}) : super(key: key);
@@ -17,6 +18,32 @@ class GameBoardView extends StatelessWidget {
       child: Center(
         child: Padding(
           padding: EdgeInsets.all(21),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              GridView.count(
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                crossAxisCount: 4,
+                children: List.generate(
+                  16,
+                  (index) => OneStringCard(
+                    cardText: index.toString(),
+                  ),
+                ),
+              ),
+              Text(
+                'Here you\'ll play the game. It needs to be implemented. ',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                  color: Colors.amber[900],
+                  fontSize: 39,
+                  backgroundColor: Colors.cyanAccent.withOpacity(0.9),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
