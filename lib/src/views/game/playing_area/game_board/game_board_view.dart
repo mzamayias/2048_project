4614c5de41c2f9ecaf75c2bad0bdd10b5c543e46
Mike Zamayias <mzamagias@gmail.com>
2021-02-11 15:36:59 +0200

    refactoring and documenting

--- lib/src/views/game/playing_area/game_board/game_board_view.dart
+++ lib/src/views/game/playing_area/game_board/game_board_view.dart
@@ -1,11 +1,13 @@
+// import Random from math
+import 'dart:math' show Random;
+// import flutter packages
 import 'package:flutter/material.dart';
-import 'package:project_2048/src/models/game/playing_area/game_board/score_model.dart';
-import 'package:project_2048/src/utlis.dart';
-import 'package:project_2048/src/views/game/playing_area/swipe_controller.dart';
-
-import '../../../../models/game/playing_area/game_board/tile_model.dart';
-
+// import custom packages
+import '../../../../utlis.dart';
+import 'swipe_controller.dart';
 import 'tile_view.dart';
+import '../../../../models/game/playing_area/game_board/score_model.dart';
+import '../../../../models/game/playing_area/game_board/tile_model.dart';
 
 class GameBoardView extends StatefulWidget {
   @override
@@ -14,16 +16,14 @@ class GameBoardView extends StatefulWidget {
 
 class GameBoardViewState extends State<GameBoardView>
     with SingleTickerProviderStateMixin {
+  // Singleton for score
   ScoreModel scoreModel = ScoreModel();
 
-  AnimationController _controller;
-  static GameBoardViewState of(BuildContext context, {bool root = false}) =>
-      root
-          ? context.findRootAncestorStateOfType<GameBoardViewState>()
-          : context.findAncestorStateOfType<GameBoardViewState>();
+  // Animation controller
+  AnimationController controller;
 
-  // 2D array of tiles.
-  List<List<TileModel>> _gameBoard = List.generate(
+  // 2D array of tiles
+  List<List<TileModel>> gameBoard = List.generate(
     4,
     (row) => List.generate(
       4,
@@ -31,51 +31,103 @@ class GameBoardViewState extends State<GameBoardView>
     ),
   );
 
-  List<TileModel> _toAdd = [];
+  // Getter for flattened game board
+  Iterable<TileModel> get flattenedGameBoard => gameBoard.expand((row) => row);
 
-  Iterable<TileModel> get _allTiles =>
-      [_flattenedGameBoard, _toAdd].expand((e) => e);
-
-  Iterable<TileModel> get _flattenedGameBoard => _gameBoard.expand((e) => e);
-
-  List<List<TileModel>> get _transposedGameBoard => List.generate(
+  // Getter for transposed game board (rows as columns, columns as rows)
+  List<List<TileModel>> get transposedGameBoard => List.generate(
         4,
         (row) => List.generate(
           4,
-          (column) => _gameBoard[column][row],
+          (column) => gameBoard[column][row],
         ),
       );
 
+  // List of tiles to add to game board
+  List<TileModel> toAdd = [];
+
+  // Getter fot all tiles in flattened game board and to add list
+  Iterable<TileModel> get allTiles =>
+      [flattenedGameBoard, toAdd].expand((tile) => tile);
+
+  // Initalize state
   @override
   void initState() {
     super.initState();
 
-    _controller = AnimationController(
+    // Animation controlller configuration
+    controller = AnimationController(
       duration: Duration(milliseconds: 300),
       vsync: this,
     );
 
-    _controller.addStatusListener((status) {
+    // Listen for changes in animation controller
+    controller.addStatusListener((status) {
       if (status == AnimationStatus.completed) {
         setState(() {
-          _toAdd.forEach(
-            (tile) => _gameBoard[tile.row][tile.column].value = tile.value,
+          toAdd.forEach(
+            (tile) => gameBoard[tile.row][tile.column].value = tile.value,
           );
-          _flattenedGameBoard.forEach(
+          flattenedGameBoard.forEach(
             (tile) => tile.resetAnimations(),
           );
-          _toAdd.clear();
+          toAdd.clear();
         });
       }
     });
+
+    // Setup new game.
     setupNewGame();
+  }
+
+  /// Add new random tiles to game board randomly.
+  void addNewTiles(List<int> values) {
+    // A list of empty tiles
+    List<TileModel> empty =
+        flattenedGameBoard.where((tile) => tile.value == 0).toList();
+    empty.shuffle();
+    for (int index = 0; index < values.length; index++) {
+      toAdd.add(
+        TileModel(
+          empty[index].row,
+          empty[index].column,
+          values[index],
+        )..appear(controller),
+      );
+    }
+  }
+
+  /// Random tile value.
+  /// There are 10% chances to get a 4 as a new tile, and
+  /// 90% chances to get a 2.
+  int randomTileValue() {
+    // Create a random number generator
+    Random rng = Random();
+    // Select a random int in range 0 inclusive, 10 exlusive.
+    // If that number is 0 (10% chance) then return 4,
+    // else (90% chance) return 2.
+    return rng.nextInt(9) == 0 ? 4 : 2;
+  }
+
+  void setupNewGame() {
+    setState(() {
+      flattenedGameBoard.forEach((tile) {
+        tile.value = 0;
+        tile.resetAnimations();
+      });
+      toAdd.clear();
+      addNewTiles([randomTileValue(), randomTileValue()]);
+      controller.forward(from: 0);
+      scoreModel.score = 0;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> _stackItems = [];
-    _stackItems.addAll(
-      _allTiles.map(
+    List<Widget> stackItems = [];
+    /// Game board with no animimations
+    stackItems.addAll(
+      allTiles.map(
         (tile) => tile.animatedValue.value == 0
             ? TileView()
             : TileView(
@@ -83,6 +135,21 @@ class GameBoardViewState extends State<GameBoardView>
               ),
       ),
     );
+    /// Game board with animimations
+    // stackItems.addAll(
+    //   allTiles.map(
+    //     (tile) => AnimatedBuilder(
+    //       animation: controller,
+    //       builder: (context, child) => tile.animatedValue.value == 0
+    //           ? TileView()
+    //           : TileView(
+    //               value: tile.animatedValue.value,
+    //             ),
+    //     ),
+    //   ),
+    // );
+
+    // Render widget
     return AspectRatio(
       aspectRatio: 1,
       child: Card(
@@ -103,10 +170,9 @@ class GameBoardViewState extends State<GameBoardView>
               mainAxisSpacing: 1.5,
               crossAxisSpacing: 1.5,
               shrinkWrap: true,
-              clipBehavior: Clip.none,
               physics: NeverScrollableScrollPhysics(),
               crossAxisCount: 4,
-              children: _stackItems,
+              children: stackItems,
             ),
           ),
         ),
@@ -117,35 +183,35 @@ class GameBoardViewState extends State<GameBoardView>
   void merge(bool Function() mergeDirection) {
     setState(() {
       if (mergeDirection()) {
-        addNewTiles([2]);
-        _controller.forward(from: 0);
+        addNewTiles([randomTileValue()]);
+        controller.forward(from: 0);
       }
     });
   }
 
   bool mergeLeft() {
-    return _gameBoard
+    return gameBoard
         .map((tile) => mergeTiles(tile))
         .toList()
         .any((tile) => tile);
   }
 
   bool mergeRight() {
-    return _gameBoard
+    return gameBoard
         .map((tile) => mergeTiles(tile.reversed.toList()))
         .toList()
         .any((tile) => tile);
   }
 
   bool mergeUp() {
-    return _transposedGameBoard
+    return transposedGameBoard
         .map((tile) => mergeTiles(tile))
         .toList()
         .any((tile) => tile);
   }
 
   bool mergeDown() {
-    return _transposedGameBoard
+    return transposedGameBoard
         .map((tile) => mergeTiles(tile.reversed.toList()))
         .toList()
         .any((tile) => tile);
@@ -153,9 +219,12 @@ class GameBoardViewState extends State<GameBoardView>
 
   bool mergeTiles(List<TileModel> tiles) {
     bool didChange = false;
+    // iterate through game board rows
     for (int row = 0; row < tiles.length; row++) {
+      // iterate through game board column
       for (int column = row; column < tiles.length; column++) {
         if (tiles[column].value != 0) {
+          // Find a tile that's not empty
           TileModel mergeTile = tiles.skip(column + 1).firstWhere(
                 (tile) => tile.value != 0,
                 orElse: () => null,
@@ -166,52 +235,23 @@ class GameBoardViewState extends State<GameBoardView>
           if (row != column || mergeTile != null) {
             didChange = true;
             int resultValue = tiles[column].value;
-            tiles[column]
-                .moveTo(_controller, tiles[row].row, tiles[row].column);
+            tiles[column].moveTo(controller, tiles[row].row, tiles[row].column);
             if (mergeTile != null) {
               resultValue += mergeTile.value;
-              mergeTile.moveTo(_controller, tiles[row].row, tiles[row].column);
-              mergeTile.bounce(_controller);
-              mergeTile.changeNumber(_controller, resultValue);
+              mergeTile.moveTo(controller, tiles[row].row, tiles[row].column);
+              mergeTile.bounce(controller);
+              mergeTile.changeNumber(controller, resultValue);
               mergeTile.value = 0;
-              tiles[column].changeNumber(_controller, 0);
+              tiles[column].changeNumber(controller, 0);
+              scoreModel.score += resultValue;
             }
             tiles[column].value = 0;
             tiles[row].value = resultValue;
-            scoreModel.score += resultValue;
           }
           break;
         }
       }
     }
     return didChange;
-  }
-
-  void addNewTiles(List<int> values) {
-    List<TileModel> empty =
-        _flattenedGameBoard.where((tile) => tile.value == 0).toList();
-    empty.shuffle();
-    for (int index = 0; index < values.length; index++) {
-      _toAdd.add(
-        TileModel(
-          empty[index].row,
-          empty[index].column,
-          values[index],
-        )..appear(_controller),
-      );
-    }
-  }
-
-  void setupNewGame() {
-    setState(() {
-      _flattenedGameBoard.forEach((tile) {
-        tile.value = 0;
-        tile.resetAnimations();
-      });
-      _toAdd.clear();
-      addNewTiles([2, 2]);
-      _controller.forward(from: 0);
-      scoreModel.score = 0;
-    });
   }
 }
