5dc5874eb46a5c0977bd67cfc17f5d8a8227cafc
Mike Zamayias <mzamagias@gmail.com>
2021-02-11 13:41:30 +0200

    removed constructor

--- lib/src/views/game/playing_area/buttons/restart_button.dart
+++ lib/src/views/game/playing_area/buttons/restart_button.dart
@@ -2,18 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:project_2048/src/views/widgets/base_button.dart';
 
 class RestartButton extends StatelessWidget {
-  const RestartButton({
-    Key key, this.onPressed,
-  }) : super(key: key);
-
-  final Function onPressed;
-
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: BaseButton(
         buttonText: 'Restart',
-        onPressed: onPressed,
       ),
     );
   }
