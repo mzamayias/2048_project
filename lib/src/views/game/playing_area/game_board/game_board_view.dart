a4145f2c41d4b4768bf990e7fc2ea65f3e028f96
Mike Zamayias <mzamagias@gmail.com>
2021-02-11 02:42:40 +0200

    game works sorta. aspectratio widget saved my head, lol.

--- lib/src/views/game/playing_area/game_board/game_board_view.dart
+++ lib/src/views/game/playing_area/game_board/game_board_view.dart
@@ -1,50 +1,207 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:project_2048/src/views/game/playing_area/game_board/tile_view.dart';
+import 'package:project_2048/src/utlis.dart';
+import 'package:project_2048/src/views/game/playing_area/swipe_controller.dart';
+
+import '../../../../models/game/playing_area/game_board/tile.dart';
+
+import 'tile_view.dart';
 
 class GameBoardView extends StatefulWidget {
-  const GameBoardView({Key key}) : super(key: key);
-
   @override
-  _GameBoardViewState createState() => _GameBoardViewState();
+  GameBoardViewState createState() => GameBoardViewState();
 }
 
-class _GameBoardViewState extends State<GameBoardView> {
+class GameBoardViewState extends State<GameBoardView>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  // 2D array of tiles.
+  List<List<Tile>> _gameBoard = List.generate(
+    4,
+    (row) => List.generate(
+      4,
+      (column) => Tile(column, row, 0),
+    ),
+  );
+
+  List<Tile> _toAdd = [];
+
+  Iterable<Tile> get _allTiles =>
+      [_flattenedGameBoard, _toAdd].expand((e) => e);
+
+  Iterable<Tile> get _flattenedGameBoard => _gameBoard.expand((e) => e);
+
+  List<List<Tile>> get _transposedGameBoard => List.generate(
+        4,
+        (row) => List.generate(
+          4,
+          (column) => _gameBoard[column][row],
+        ),
+      );
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: Duration(milliseconds: 210),
+      vsync: this,
+    );
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        setState(() {
+          _toAdd.forEach(
+            (tile) => _gameBoard[tile.column][tile.row].value = tile.value,
+          );
+          _flattenedGameBoard.forEach(
+            (tile) => tile.resetAnimations(),
+          );
+          _toAdd.clear();
+        });
+      }
+    });
+    setupNewGame();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(21)),
+    List<Widget> _stackItems = [];
+    _stackItems.addAll(
+      _allTiles.map(
+        (tile) => AnimatedBuilder(
+          animation: _controller,
+          builder: (context, child) => tile.animatedValue.value == 0
+              ? TileView()
+              : TileView(
+                  value: tile.animatedValue.value,
+                ),
+        ),
       ),
-      color: Colors.cyan[700].withOpacity(0.9),
-      shadowColor: Colors.grey[850],
-      elevation: 3,
-      child: Center(
+    );
+    return AspectRatio(
+      aspectRatio: 1,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: borderRadius,
+        ),
+        color: gameBoardBackgroundColor,
+        shadowColor: shadowColor,
+        elevation: elevation,
         child: Padding(
-          padding: EdgeInsets.all(9),
-          child: GridView.count(
-            physics: NeverScrollableScrollPhysics(),
-            shrinkWrap: true,
-            crossAxisCount: 4,
-            children: List.generate(
-              16,
-              (index) {
-                int powerOfTwo = pow(2, index);
-                if (2 <= powerOfTwo && powerOfTwo <= 2048) {
-                  return TileView(
-                    value: powerOfTwo,
-                  );
-                } else {
-                  return TileView(
-                    value: null,
-                  );
-                }
-              },
+          padding: EdgeInsets.all(6),
+          child: SwipeController(
+            up: () => merge(mergeUp),
+            down: () => merge(mergeDown),
+            left: () => merge(mergeLeft),
+            right: () => merge(mergeRight),
+            child: GridView.count(
+              mainAxisSpacing: 1.5,
+              crossAxisSpacing: 1.5,
+              shrinkWrap: true,
+              clipBehavior: Clip.none,
+              physics: NeverScrollableScrollPhysics(),
+              crossAxisCount: 4,
+              children: _stackItems,
             ),
           ),
         ),
       ),
     );
+  }
+
+  void merge(bool Function() mergeDirection) {
+    setState(() {
+      if (mergeDirection()) {
+        addNewTiles([2]);
+        _controller.forward(from: 0);
+      }
+    });
+  }
+
+  bool mergeLeft() {
+    return _gameBoard.map((e) => mergeTiles(e)).toList().any((e) => e);
+  }
+
+  bool mergeRight() {
+    return _gameBoard
+        .map((e) => mergeTiles(e.reversed.toList()))
+        .toList()
+        .any((e) => e);
+  }
+
+  bool mergeUp() {
+    return _transposedGameBoard
+        .map((e) => mergeTiles(e))
+        .toList()
+        .any((e) => e);
+  }
+
+  bool mergeDown() {
+    return _transposedGameBoard
+        .map((e) => mergeTiles(e.reversed.toList()))
+        .toList()
+        .any((e) => e);
+  }
+
+  bool mergeTiles(List<Tile> tiles) {
+    bool didChange = false;
+    for (int row = 0; row < tiles.length; row++) {
+      for (int column = row; column < tiles.length; column++) {
+        if (tiles[column].value != 0) {
+          Tile mergeTile = tiles
+              .skip(column + 1)
+              .firstWhere((tile) => tile.value != 0, orElse: () => null);
+          if (mergeTile != null && mergeTile.value != tiles[column].value) {
+            mergeTile = null;
+          }
+          if (row != column || mergeTile != null) {
+            didChange = true;
+            int resultValue = tiles[column].value;
+            tiles[column]
+                .moveTo(_controller, tiles[row].row, tiles[row].column);
+            if (mergeTile != null) {
+              resultValue += mergeTile.value;
+              mergeTile.moveTo(_controller, tiles[row].row, tiles[row].column);
+              mergeTile.bounce(_controller);
+              mergeTile.changeNumber(_controller, resultValue);
+              mergeTile.value = 0;
+              tiles[column].changeNumber(_controller, 0);
+            }
+            tiles[column].value = 0;
+            tiles[row].value = resultValue;
+          }
+          break;
+        }
+      }
+    }
+    _controller.forward(from: 0);
+    return didChange;
+  }
+
+  void addNewTiles(List<int> values) {
+    List<Tile> empty =
+        _flattenedGameBoard.where((tile) => tile.value == 0).toList();
+    empty.shuffle();
+    for (int index = 0; index < values.length; index++) {
+      _toAdd.add(
+        Tile(
+          empty[index].row,
+          empty[index].column,
+          values[index],
+        )..appear(_controller),
+      );
+    }
+    _controller.forward(from: 0);
+  }
+
+  void setupNewGame() {
+    setState(() {
+      _flattenedGameBoard.forEach((tile) {
+        tile.value = 0;
+        tile.resetAnimations();
+      });
+      _toAdd.clear();
+      addNewTiles([2, 2]);
+      _controller.forward(from: 0);
+    });
   }
 }
--- lib/src/views/game/playing_area/playing_area_view.dart
+++ lib/src/views/game/playing_area/playing_area_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart' hide BackButton;
 import 'package:project_2048/src/views/game/playing_area/buttons/redo_button.dart';
 import 'package:project_2048/src/views/game/playing_area/buttons/restart_button.dart';
 import 'package:project_2048/src/views/game/playing_area/game_board/game_board_view.dart';
+// import 'package:project_2048/src/views/game/playing_area/game_board/game_board_view.dart';
 import 'package:project_2048/src/views/game/playing_area/score_board/score_board_view.dart';
 import 'package:project_2048/src/views/widgets/back_button.dart';
 import 'package:project_2048/src/views/widgets/base_view.dart';
@@ -14,15 +15,8 @@ class PlayingAreaView extends StatelessWidget {
     return BaseView(
       viewTitle: '2048',
       children: [
-        Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            RestartButton(),
-            RedoButton(),
-          ],
-        ),
-        Spacer(),
+        RestartButton(),
+        RedoButton(),
         GameBoardView(),
         ScoreBoard(),
         BackButton(),
