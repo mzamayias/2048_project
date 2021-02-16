7035af0292b8d8d180c8cc7008f69a25dc96c102
Mike Zamayias <mzamagias@gmail.com>
2021-02-16 07:43:54 +0200

    fixed: unnecessary_getters_setters

--- lib/src/models/help/about/about_model.dart
+++ lib/src/models/help/about/about_model.dart
@@ -2,25 +2,13 @@ import 'app.dart';
 import 'developer.dart';
 
 class AboutModel {
-  Developer _developer = Developer(
+  final Developer developer = Developer(
     firstName: 'Mike',
     lastName: 'Zamayias',
   );
 
-  App _app = App(
+  final App app = App(
     name: '2048 Semester Project',
     version: 'Still Under Developement',
   );
-
-  Developer get developer => _developer;
-
-  set developer(Developer value) {
-    _developer = value;
-  }
-
-  App get app => _app;
-
-  set app(App value) {
-    _app = value;
-  }
 }
