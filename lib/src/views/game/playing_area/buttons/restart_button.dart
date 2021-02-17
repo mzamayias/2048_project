24a09ab115bd4ca565684d0587d38fe168e2a7c4
Mike Zamayias <mzamagias@gmail.com>
2021-02-17 06:26:16 +0200

    trying to implement the restart logic

--- lib/src/views/game/playing_area/buttons/restart_button.dart
+++ lib/src/views/game/playing_area/buttons/restart_button.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:project_2048/src/views/widgets/base_button.dart';
+import 'package:project_2048/src/controllers/game/playing_area/game_board/restart_controller.dart';
+import 'package:provider/provider.dart';
 
 class RestartButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Expanded(
+    final RestartController restartController =
+        Provider.of<RestartController>(context);
+    return Expanded(
       child: BaseButton(
         buttonText: 'Restart',
+        onPressed: () {
+          debugPrint(
+              'old restart controller value: ${restartController.value}');
+          restartController.value = true;
+          debugPrint(
+              'new restart controller value: ${restartController.value}');
+        },
       ),
     );
   }
